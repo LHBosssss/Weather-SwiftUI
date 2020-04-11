c4d2106a81f84dcd704e9d931b44e0a670c04cf0
Luong Ho <hoduyluong2108@gmail.com>
2020-04-11 20:47:04 +0700

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Ho Duy Luong on 4/11/20.
+//  Copyright © 2020 Ho Duy Luong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
